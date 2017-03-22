c0656389e9d68f3dc0f49bd518b24a397b751e18
Claudia Tnt <namting88@gmail.com>
2017-03-22 18:00:14 +0800

    1. add gem clearance for user to signup, sign in and sign out with protected password. 2. add gem figaro for privacy protection. 3. add gem OAuth for Facebook authorization.

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'clearance'
+gem 'figaro'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  has_many :authentications, :dependent => :destroy
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+      # byebug
+      user = User.create!(firstname: auth_hash["info"]["first_name"], lastname: auth_hash["info"]["last_name"], email: auth_hash["info"]["email"])
+      user.authentications << (authentication)
+      return user
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
+  def password_optional?
+    true
+  end
+
+  # or can put a random password: '1234' in line 8
+
 end
