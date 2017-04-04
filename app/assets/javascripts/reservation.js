664af959df90b43a91a96c24f5e7508c6801fb9b
Claudia Tnt <namting88@gmail.com>
2017-04-04 16:32:21 +0800

    Gem sidekiq and Redis for background email sending

--- app/assets/javascripts/reservation.js
+++ app/assets/javascripts/reservation.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+// turbolinks:load is to makes sure turbolinks work while moving on to the next page which are using datepicker. Becasue if didn't turn it on, turbolinks will crash the datepicker and it can't be use.
+$( document ).on('turbolinks:load', function(){
   console.log('hi');
   $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
 });
---
+++ app/jobs/reservation_job.rb
@@ -0,0 +1,9 @@
+class ReservationJob < ApplicationJob
+  queue_as :default
+
+  # def perform(customer_id, host_id, reservation_id)
+  #   ReservationMailer.booking_email_to_customer(customer_id, host_id, reservation_id)
+  #   ReservationMailer.booking_email_to_host(customer_id, host_id, reservation_id)
+  # end
+
+end
